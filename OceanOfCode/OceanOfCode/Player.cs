8c63f3c753db4dbf5dec395a938edac5c442ad5e
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-10-23 13:34:10 -0300

    Ocean of Code Init
    https://www.codingame.com/ide/puzzle/ocean-of-code

---
+++ OceanOfCode/OceanOfCode/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OceanOfCode
+{
+    class Player
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
