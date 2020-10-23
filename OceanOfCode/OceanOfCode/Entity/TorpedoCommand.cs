71ca8de2e59f4cdbd9a7fca9da82ab2853e8187f
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-10-23 17:02:13 -0300

    Command to send torpedo

---
+++ OceanOfCode/OceanOfCode/Entity/TorpedoCommand.cs
@@ -0,0 +1,20 @@
+﻿using OceanOfCode.Interfaces;
+using System.Drawing;
+
+namespace OceanOfCode.Entity
+{
+    public class TorpedoCommand : ICommand
+    {
+        public Point Position { get; set; }
+
+        public TorpedoCommand(Point position)
+        {
+            Position = position;
+        }
+
+        public string Execute()
+        {
+            return $"TORPEDO {Position.X} {Position.Y}";
+        }
+    }
+}
